28a5512dbf2517e2d891a12924cc424aafa93737
Chad West <chad@chadmwest.com>
2016-11-06 19:46:01 -0600

    Removed head message inserted by git.

--- BillShareApp/app/src/main/java/billshare/com/restservice/RestServiceObject.java
+++ BillShareApp/app/src/main/java/billshare/com/restservice/RestServiceObject.java
@@ -13,11 +13,10 @@ import retrofit.Retrofit;
 public class RestServiceObject {
     private static Context context1;
 
-<<<<<<< HEAD
-    private static final String BASE_URL = "http://192.168.0.101:8080";
-=======
+    //private static final String BASE_URL = "http://192.168.0.101:8080";
+
     private static final String BASE_URL = "http://192.168.0.21:8080";
->>>>>>> 53b57b7ebf2afcfe63ce985ac80391b510b5399f
+
 
     public static IRestServices getiRestServicesObject(Context context) {
         context1 = context;
