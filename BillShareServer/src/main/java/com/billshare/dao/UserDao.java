b04eff810199912ee2df4a98ceca4b961e0fadd7
anusha <anushareddii@gmail.com>
2016-11-29 09:01:12 -0600

    password encription

--- BillShareServer/src/main/java/com/billshare/dao/UserDao.java
+++ BillShareServer/src/main/java/com/billshare/dao/UserDao.java
@@ -1,8 +1,16 @@
 package com.billshare.dao;
 
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
 import java.util.List;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
@@ -23,16 +31,52 @@ public class UserDao {
 	private EntityManager entityManager;
 
 	public boolean register(User user) {
+		user.setPassword(getEncrypted(user.getPassword()));
 		entityManager.persist(user);
 		return true;
 	}
+private String getEncrypted(String pwd){
+	String key = "Bar12345Bar12345";
+	Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
+	Cipher cipher;
+	try {
+		cipher = Cipher.getInstance("AES");
+		cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+		byte[] encrypted = cipher.doFinal(pwd.getBytes());
+		
+		 StringBuilder sb = new StringBuilder();
+            for (byte b: encrypted) {
+                sb.append((char)b);
+            }
 
+            String enc = sb.toString();
+            return enc;
+	} catch (NoSuchAlgorithmException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (NoSuchPaddingException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (InvalidKeyException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IllegalBlockSizeException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (BadPaddingException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} 
+	return null;
+}
 	public User login(User user) {
+		user.setPassword(getEncrypted(user.getPassword()));
 		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 		CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
 		Root<User> root = criteriaQuery.from(User.class);
 		criteriaQuery.select(root);
 		try {
+			
 			Predicate equalPassword = criteriaBuilder.equal(root.get("password"), user.getPassword());
 			Predicate equalUserName = criteriaBuilder.equal(root.get("emailId"), user.getEmailId());
 
